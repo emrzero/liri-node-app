de52199dafe3d7afe58a5b7af4bd6ea17ada6978
Eliot Reyes <eliotmij@gmail.com>
2016-09-01 18:57:47 -0700

    my-tweets

--- liri.js
+++ liri.js
@@ -1,9 +1,34 @@
 var fs = require('fs');
-var req = require('request');
+var myRequest = require('request');
 var keys = require('./keys.js');
+var Twitter = require('twitter');
+
+keys.bearer_token_credentials = keys.twitterKeys.consumer_key + ":" + keys.twitterKeys.consumer_secret;
+// console.log(new Buffer('Hello World').toString('base64'));
+keys.Base64_encoded_btc = new Buffer(keys.bearer_token_credentials).toString('base64'); 
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret,
+  count: 20,
+  trim_user: 1
+});
+
+client.get('statuses/user_timeline', function(error,tweets, response){
+  if(error) throw error;
+  // console.log(JSON.stringify(tweets, null, 2));  // The favorites. 
+  // console.log(JSON.stringify(response, null, 2));  // Raw response object.
+
+  tweets.forEach(function(element){
+    console.log(element.created_at);
+    console.log(element.text);
+  });
+});
 
 var newArr = process.argv.slice(2);
-  // * `my-tweets`
+  // * `my-tweets`  DONE
 
   // * `spotify-this-song`
 
